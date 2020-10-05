815cf155e98c859accdf30a9c9cdd9b8c2ed59f5
amarsmann1224 <70251359+amarsmann1224@users.noreply.github.com>
2020-10-04 19:27:35 -0700

    Add files via upload

---
+++ markov.py
@@ -0,0 +1,11 @@
+import numpy as np
+from numpy.linalg import matrix_power
+P=np.matrix([[0.25,.40],[.75,.60]])
+print(P)
+N=np.matrix([[50],[50]])
+print(N)
+N=P*N
+print(N)
+P10=matrix_power(P,10)
+N=P10*N
+print(N)
